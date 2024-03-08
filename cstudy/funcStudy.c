9b7b8b3135170193658d4affdf96c286c4b8d13c
kyerim1 <nilzero25@gmail.com>
2024-03-08 09:35:32 +0900

    야바위

--- cstudy/funcStudy.c
+++ cstudy/funcStudy.c
@@ -4,31 +4,68 @@
 
 //   Sleep(1000);
 
-void draw();  //그림 그리는 함수
-int game();  //게임 시작함수
+int init(); // 게임 설정
+void draw(int gamer);  //그림 그리는 함수
+int game(int gamer);  //게임 시작함수
 void result(int select);  //결과 출력함수
 
 void main() {
+	int gamer = init();
 	int select = 0;
 	printf("야바위 게임 함수호출");
-	select = game();
+	select = game(gamer);
 	result(select);
 }
-void draw() {
-	printf("┌──────┐     ┌──────┐     ┌──────┐     ┌──────┐\n");
-	printf("│      │     │      │     │      │     │      │\n");
-	printf("│   1  │     │   2  │     │   3  │     │   4  │\n");
-	printf("│      │     │      │     │      │     │      │\n");
-	printf("└──────┘     └──────┘     └──────┘     └──────┘\n");
+int init() {
+	int cnt = 0,people=0;
+	do {
+		printf("참가인원 설정 : ");
+		scanf_s("%d", &people);
+		if (people > 7) printf(" 최대 참가 인원수는 7명 입니다.");
+	} while (people > 7);
+	cnt = people / 2 + 2;
+	return cnt;
 }
-int game() {
+void draw(int gamer) {
+	
+	for (int i = 1; i <= gamer; i++) {
+		printf("┌──────┐");
+		if (i != gamer) printf("     ");
+	}
+	printf("\n");
+	for (int i = 1; i <= gamer; i++) {
+		printf("│      │");
+		if (i != gamer) printf("     ");
+	}
+	printf("\n");
+	for (int i = 1; i <= gamer; i++) {
+		printf("│   %d  │",i);
+		if (i != gamer) printf("     ");
+	}
+	printf("\n");
+	for (int i = 1; i <= gamer; i++) {
+		printf("│      │");
+		if (i != gamer) printf("     ");
+	}
+	printf("\n");
+	for (int i = 1; i <= gamer; i++) {
+		printf("└──────┘");
+		if (i != gamer) printf("     ");
+	}
+	printf("\n");
+
+
+
+
+}
+int game(int gamer) {
 	int select;
 	printf("자~아 돈 놓고 돈 먹기 \n");
 	printf("지나가는 개도 다 맞추는 그 게임\n\n");
 	printf("'그리기 함수 호출중");
 	for (int i = 1; i <= 10; i++) { Sleep(500); printf("."); }
 	printf("'\n");
-	draw();
+	draw(gamer);
 	printf("\n왕을 찍으세요 왕!!!! : ");
 	scanf_s("%d", &select);
 	return select;
